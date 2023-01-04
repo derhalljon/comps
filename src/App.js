0a147b8c7dbbeab06e99496c94f33a4e1a566c15
HalldorjonD <77391518+derhalljon@users.noreply.github.com>
2023-01-04 15:53:37 +0000

    intro to prop types

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Button from "./Button";
+
+function App() {
+    return <div>
+        <div>
+            <Button primary>Button Cheese</Button>
+        </div>
+        <div>
+            <Button>Cheese Button</Button>
+        </div>
+        <div>
+            <Button>Ribs</Button>
+        </div>
+        <div>
+            <Button>Chocolate</Button>
+        </div>
+        <div>
+            <Button>Sweetcorn</Button>
+        </div>
+        <div>
+            <Button>Churros</Button>
+        </div>
+        <div>
+            <Button>A small piece of potato</Button>
+        </div>
+    </div>;
+
+}
+
+
+export default App;
