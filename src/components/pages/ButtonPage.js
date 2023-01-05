73d2003fe757a1c4c52173128d8ea11682adc1c3
HalldorjonD <77391518+derhalljon@users.noreply.github.com>
2023-01-05 12:45:06 +0000

    reorganised files

--- src/components/pages/ButtonPage.js
+++ src/components/pages/ButtonPage.js
@@ -1,7 +1,7 @@
 import { RiBuilding3Fill, RiHomeSmileLine, RiAttachmentLine, RiCopyrightFill } from "react-icons/ri";
-import Button from "./components/Button";
+import Button from "../components/Button";
 
-function App() {
+function ButtonPage() {
     return (
         <div>
             <div>
@@ -49,4 +49,4 @@ function App() {
 }
 
 
-export default App;
+export default ButtonPage;
